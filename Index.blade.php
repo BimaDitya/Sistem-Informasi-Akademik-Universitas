b113763c9388413cfe7cacc34fe46784dfb3bb9b
Vira Aditya S.P <91040431+ViraASP@users.noreply.github.com>
2021-11-17 13:55:22 +0700

    Transkrip
    Penambahan footer

---
+++ Index.blade.php
@@ -0,0 +1,158 @@
+@extends('Admin.MainAdmin')
+@section('Contents')
+{{-- Tab --}}
+<ul class="nav nav-tabs nav-fill" id="pills-tab" role="tablist">
+    <li class="nav-item" role="presentation">
+        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home"
+            type="button" role="tab" aria-controls="pills-home" aria-selected="true">Daftar Perkuliahan</button>
+    </li>
+    <li class="nav-item" role="presentation">
+        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile"
+            type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Tambah Data</button>
+    </li>
+</ul>
+{{-- Tab --}}
+
+{{-- Content --}}
+<div class="tab-content" id="pills-tabContent">
+    {{-- Data Transkrip --}}
+    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+        <table class="table table-bordered">
+            <thead>
+                <tr class="table-secondary text-center">
+                    <th>NIM</th>
+                    <th>Matakuliah</th>
+                    <th>SKS</th>
+                    <th>Nilai</th>
+                    <th>Indeks</th>
+                    <th style="width: 10REM">Perintah</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($Grades as $Item)
+                <tr class="table-light text-center align-middle">
+                    <td>{{ $Item->account->no_id ?? '' }}</td>
+                    <td>{{ $Item->course->matakuliah ?? ''}}</td>
+                    <td>{{ $Item->sks ?? ''}}</td>
+                    <td>{{ $Item->nilai ?? ''}}</td>
+                    <td>{{ $Item->indeks ?? ''}}</td>
+                    <td>
+                        <div class="row justify-content-center">
+                            <div class="col">
+                                <a href="/Admin/Transkrip/{{ $Item->id }}" class="btn btn-warning text-decoration-none">
+                                    <i class="ph-pencil-simple align-middle"></i>
+                                </a>
+                            </div>
+                            <form action="/Admin/Transkrip/Hapus/{{ $Item->id }}" class="col" method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <button class="btn btn-danger" onclick="return confirm('Anda Yakin Menghapus Data?')">
+                                    <i class="ph-trash-simple align-middle"></i>
+                                </button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @if (session()->has('Berhasil'))
+        <div class="alert alert-success alert-dismissible fade show my-2" role="alert">
+            {{ session('Berhasil') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        @endif
+        @if ($errors->any())
+        <div class="alert alert-danger alert-dismissible fade show my-2" role="alert">
+            Gagal Menyimpan Data
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        @endif
+    </div>
+    {{-- Tambah Data --}}
+    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+        {{-- Grade Data --}}
+        <div class="row justify-content-center">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header text-center highlight-font fw-bold">
+                        Data Transkrip Nilai
+                    </div>
+                    <div class="card-body shadow-sm">
+                        <form method="POST" action="/Admin/Transkrip/">
+                            @csrf
+                            <div class="mb-1">
+                                <label for="matakuliah" class="col-sm-4 col-form-label text-start">Nama
+                                    Matakuliah</label>
+                                <select class="form-select" name="course_id">
+                                    <option value="">Pilih Matakuliah</option>
+                                    @foreach ($Courses as $Item)
+                                        <option value="{{ $Item->id }}" {{ old('course_id')  == $Item->id ? 'selected' : null}}>{{ $Item->matakuliah }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="mb-1">
+                                <label for="nim" class="col-sm-4 col-form-label text-start">Nomor Induk
+                                    Mahasiswa</label>
+                                <select class="form-select" name="account_id">
+                                    <option value="">Pilih Nomor Induk Mahasiswa</option>
+                                    @foreach ($Accounts as $Item)
+                                        <option value="{{ $Item->id }}" {{ old('account_id') == $Item->id ? 'selected' : null}}>{{ $Item->no_id }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="mb-2">
+                                <label for="sks" class="col-sm-4 col-form-label text-start">Satuan Kredit
+                                    Semester</label>
+                                <input type="number" name="sks" class="form-control" id="sks" placeholder="Contoh: 3" value="{{ old('sks') }}">
+                            </div>
+                            <div class="row mb-2">
+                                <div class="col">
+                                    <label for="nilai" class="col-lg-4 col-form-label text-start">Nilai
+                                        Matakuliah</label>
+                                    <select class="form-select" name="nilai">
+                                        <option value="">Tentukan Nilai</option>
+                                        <option value="A" {{ old('nilai') == "A" ? 'selected' : null}}>A</option>
+                                        <option value="A-" {{ old('nilai') == "A-" ? 'selected' : null}}>A-</option>
+                                        <option value="B+" {{ old('nilai') == "B+" ? 'selected' : null}}>B+</option>
+                                        <option value="B" {{ old('nilai') == "B" ? 'selected' : null}}>B</option>
+                                        <option value="B-" {{ old('nilai') == "B-" ? 'selected' : null}}>B-</option>
+                                        <option value="C+" {{ old('nilai') == "C+" ? 'selected' : null}}>C+</option>
+                                        <option value="C" {{ old('nilai') == "C" ? 'selected' : null}}>C</option>
+                                        <option value="C-" {{ old('nilai') == "C-" ? 'selected' : null}}>C-</option>
+                                        <option value="D" {{ old('nilai') == "D" ? 'selected' : null}}>D</option>
+                                        <option value="E" {{ old('nilai') == "E" ? 'selected' : null}}>E</option>
+                                    </select>
+                                </div>
+                                <div class="col">
+                                    <label for="indeks" class="col-lg-4 col-form-label text-start">Indeks
+                                        Matakuliah</label>
+                                    <select class="form-select" name="indeks">
+                                        <option value="">Tentukan Indeks</option>
+                                        <option value="4.00">4.00</option>
+                                        <option value="3.75">3.75</option>
+                                        <option value="3.50">3.50</option>
+                                        <option value="3.00">3.00</option>
+                                        <option value="2.75">2.75</option>
+                                        <option value="2.50">2.50</option>
+                                        <option value="2.25">2.25</option>
+                                        <option value="2.00">2.00</option>
+                                        <option value="1.00">1.00</option>
+                                        <option value="0">0</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Simpan Data</button>
+                            <button class="btn btn-secondary" type="reset">Hapus</button>
+                        </form>
+                                            <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="https://www.SINIKAD.com">Kampus</a> | <span class="far fa-copyright"></span> 2021 All rights reserved.</span>
+    </footer>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ Update.blade.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>SINIKAD | {{ $Title }} Page</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Custom Style -->
+    <link href='{{ asset('Style/AdminStyle.css') }}' rel="stylesheet">
+</head>
+
+<body>
+    {{-- Content --}}
+    <div class="row h-100 justify-content-center align-items-center py-4">
+        <div class="col-lg-6 vertical-center">
+            <div class="card shadow">
+                <div class="card-header text-center fw-bold highlight-font">
+                    Ubah Data Nilai
+                </div>
+                <div class="card-body">
+                    <a href="/Admin/Transkrip" class="btn btn-primary">Kembali</a>
+                    <form method="POST" action="/Admin/Transkrip/Update/{{ $Grades->id }}">
+                        @csrf
+                        <div class="mb-1">
+                            <label for="matakuliah" class="col-sm-4 col-form-label text-start">Nama
+                                Matakuliah</label>
+                            <select class="form-select" name="course_id">
+                                <option value="">Pilih Matakuliah</option>
+                                @foreach ($Courses as $Item)
+                                <option value="{{ $Item->id }}" {{ old('course_id', $Grades->course_id)==$Item->id ? 'selected' : null}}>{{ $Item->matakuliah }}
+                                </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="mb-1">
+                            <label for="nim" class="col-sm-4 col-form-label text-start">Nomor Induk
+                                Mahasiswa</label>
+                            <select class="form-select" name="account_id">
+                                <option value="">Pilih Nomor Induk Mahasiswa</option>
+                                @foreach ($Accounts as $Item)
+                                <option value="{{ $Item->id }}" {{ old('account_id', $Grades->account_id)==$Item->id ? 'selected' : null}}>{{ $Item->nim }}
+                                </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="mb-1">
+                            <label for="sks" class="col-sm-5 col-form-label text-start">Satuan Kredit
+                                Semester</label>
+                            <input type="number" name="sks" class="form-control" id="sks" placeholder="Contoh: 3" value="{{ old('sks', $Grades->sks) }}">
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <label for="nilai" class="col-sm-6 col-form-label text-start">Nilai
+                                    Matakuliah</label>
+                                <select class="form-select" name="nilai">
+                                    <option value=""></option>
+                                    <option value="A" {{ old('nilai', $Grades->nilai)=="A" ? 'selected' : null}}>A</option>
+                                    <option value="A-" {{ old('nilai', $Grades->nilai)=="A-" ? 'selected' : null}}>A-</option>
+                                    <option value="B+" {{ old('nilai', $Grades->nilai)=="B+" ? 'selected' : null}}>B+</option>
+                                    <option value="B" {{ old('nilai', $Grades->nilai)=="B" ? 'selected' : null}}>B</option>
+                                    <option value="B-" {{ old('nilai', $Grades->nilai)=="B-" ? 'selected' : null}}>B-</option>
+                                    <option value="C+" {{ old('nilai', $Grades->nilai)=="C+" ? 'selected' : null}}>C+</option>
+                                    <option value="C" {{ old('nilai', $Grades->nilai)=="C" ? 'selected' : null}}>C</option>
+                                    <option value="C-" {{ old('nilai', $Grades->nilai)=="C-" ? 'selected' : null}}>C-</option>
+                                    <option value="D" {{ old('nilai', $Grades->nilai)=="D" ? 'selected' : null}}>D</option>
+                                    <option value="E" {{ old('nilai', $Grades->nilai)=="E" ? 'selected' : null}}>E</option>
+                                </select>
+                            </div>
+                            <div class="col">
+                                <label for="indeks" class="col-sm-6 col-form-label text-start">Indeks
+                                    Matakuliah</label>
+                                <select class="form-select" name="indeks">
+                                    <option value=""></option>
+                                    <option value="4.00" {{ old('indeks', $Grades->indeks)=="4.00" ? 'selected' : null}}>4.00</option>
+                                    <option value="3.75" {{ old('indeks', $Grades->indeks)=="3.75" ? 'selected' : null}}>3.75</option>
+                                    <option value="3.50" {{ old('indeks', $Grades->indeks)=="3.50" ? 'selected' : null}}>3.50</option>
+                                    <option value="3.00" {{ old('indeks', $Grades->indeks)=="3.00" ? 'selected' : null}}>3.00</option>
+                                    <option value="2.75" {{ old('indeks', $Grades->indeks)=="2.75" ? 'selected' : null}}>2.75</option>
+                                    <option value="2.50" {{ old('indeks', $Grades->indeks)=="2.50" ? 'selected' : null}}>2.50</option>
+                                    <option value="2.25" {{ old('indeks', $Grades->indeks)=="2.25" ? 'selected' : null}}>2.25</option>
+                                    <option value="2.00" {{ old('indeks', $Grades->indeks)=="2.00" ? 'selected' : null}}>2.00</option>
+                                    <option value="1.00" {{ old('indeks', $Grades->indeks)=="1.00" ? 'selected' : null}}>1.00</option>
+                                    <option value="0" {{ old('indeks', $Grades->indeks)=="0" ? 'selected' : null}}>0</option>
+                                </select>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-success">Update Data</button>
+                    </form>
+                </div>
+            </div>
+            @if ($errors->any())
+            <div class="alert alert-danger alert-dismissible fade show my-2" role="alert">
+                Gagal Menyimpan Data, Periksa Kembali
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            @endif
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+        </script>
+                    <!-- footer section start -->
+                    <footer>
+                        <span>Created By <a href="https://www.SINIKAD.com">Kampus</a> | <span class="far fa-copyright"></span> 2021 All rights reserved.</span>
+                    </footer>
+</body>
+
+
+</html>
